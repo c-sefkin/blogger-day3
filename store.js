f0a690ae8f52a2d56823c028bbc6c642ca88b9d0
Chris Sefkin <c.sefkin@gmail.com>
2015-06-09 11:10:10 -0700

    comments

---
+++ store.js
@@ -0,0 +1,15 @@
+var posts = [
+  {
+    id: '1',
+    title: "I love blood",
+    body: "I love blood!"
+  },
+];
+
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'What a shame!'
+  },
+];
